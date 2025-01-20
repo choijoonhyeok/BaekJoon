e7053428e8319c8a3cb07f5dec106b85bca5e20f
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2025-01-20 20:21:04 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+    	int a = s.nextInt();
+ 
+    	
+    	if(a>=90) System.out.print("A");
+    	else if(a>=80)System.out.println("B");
+    	else if(a>=70)System.out.println("C");
+    	else if(a>=60)System.out.println("D");
+    	else System.out.println("F");
+    }
+}
