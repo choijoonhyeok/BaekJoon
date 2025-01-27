2f2a0d45da2cde08b65b8c1e2ef38bfa038c2bb1
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2025-01-27 14:37:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2324 ms, Memory: 357348 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,35 +1,24 @@
-import java.util.HashMap;
 import java.util.Scanner;
+import java.util.HashMap;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // 첫 번째 입력: 상근이가 가지고 있는 숫자 카드의 개수 N
-        int N = sc.nextInt();
-
-        // 숫자 카드 개수를 저장할 HashMap
-        HashMap<Integer, Integer> cardMap = new HashMap<>();
-
-        // 두 번째 입력: 숫자 카드에 적힌 정수
-        for (int i = 0; i < N; i++) {
-            int card = sc.nextInt();
-            cardMap.put(card, cardMap.getOrDefault(card, 0) + 1);
+        Scanner s = new Scanner(System.in);
+          
+        int fn = s.nextInt();
+        HashMap<Integer,Integer> ahash = new HashMap <>(); 
+        for (int i=0; i<fn;i++) {
+        	int aint =s.nextInt();
+        	ahash.put(aint,ahash.getOrDefault(aint,0)+1);
         }
-
-        // 세 번째 입력: 확인할 숫자 카드의 개수 M
-        int M = sc.nextInt();
-
-        // 네 번째 입력: M개의 정수
+        int sn = s.nextInt();
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int query = sc.nextInt();
-            result.append(cardMap.getOrDefault(query, 0)).append(" ");
+        for(int i=0;i<sn;i++) {
+        	int sint = s.nextInt();
+        	result.append(ahash.getOrDefault(sint,0)).append(" ");
         }
-
-        // 결과 출력
-        System.out.println(result.toString().trim());
-
-        sc.close();
+        System.out.println(result.toString());
+        
     }
 }
