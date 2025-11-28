b758cf28bffabb1b2023ae6abadcc3f856859195
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2025-11-29 01:03:08 +0900

    [level 2] Title: 전화번호 목록, Time: 293.47 ms, Memory: 217 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,27 +1,53 @@
-import java.util.Scanner;
-import java.util.HashSet;
+// import java.util.*;
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+//         boolean answer = true;
+//          Arrays.sort(phone_book);
+//         Set <String> set = new HashSet<>();
+        
+//         for(String phone : phone_book){
+//             set.add(phone);
+//              for(int i=0;i< phone_book.length;i++){ 
+//                  for(int j=0;j<phone.length();j++){
+//                      if(phone_book[i].equals(phone.substring(0,j))) {
+//                         System.out.println(answer);
+//                          return false;
+                         
+                       
+                         
+//                      };
+                     
+//                  }
+//              }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        HashSet<String> hm = new HashSet<>();
-        for(String number:phone_book){
-            hm.add(number);
+		    //✅ 해시셋에 phone_book의 전화번호를 모두 저장한다.
+	    	Set<String> prefixSet = new HashSet<>();
+        for (String number : phone_book) {
+            prefixSet.add(number);
         }
         
-        for(String number:phone_book){
-            for(int i=1; i<number.length();i++){
-                if(hm.contains(number.substring(0,i))) return false;
+        	    	
+	    	//✅ phone_book의 모든 전화번호를 순회한다.
+        for (String number : phone_book) {
+        		//✅ i를 1 ~ n까지 순회하며 0 ~ i 까지 부분문자열을 구한다.
+        	  for (int i = 1; i < number.length(); i++) {
+    	  				//✅ 부분문자열이 해시셋에 있다면 false를 반환한다.
+                String sub = number.substring(0, i);
+                if (prefixSet.contains(sub)) return false;
             }
         }
+        
+        //✅ 접두어를 찾지 못했다면 true를 반환한다.
         return true;
-    }
-    public static void main(String[]args){
-        Scanner s = new Scanner(System.in);
-        
-        String str = s.nextLine();
-        str=str.replace("[\"","").replace("\"]","");
-        String phone_book[] = str.split("\", \"");
-        
-        Solution sol = new Solution();
-        System.out.print(sol.solution(phone_book));
     }
 }
