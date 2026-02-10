01d9be78bc09d0b816bac7778c2c1c53cf380e97
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2026-02-11 01:29:46 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.99 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,25 @@
 import java.util.*;
+
 class Solution {
     public String solution(String s) {
         StringBuilder answer = new StringBuilder();
-        int idx = 0;
+        String array[] = s.split(" ", -1); // 공백 유지
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for (int i = 0; i < array.length; i++) {
+            String arr[] = array[i].split("");
 
-            if (c == ' ') {
-                answer.append(c);
-                idx = 0;
-            } else {
-                if (idx % 2 == 0) {
-                    answer.append(Character.toUpperCase(c));
+            for (int j = 0; j < arr.length; j++) {
+                if (j % 2 == 0) {
+                    arr[j] = arr[j].toUpperCase();
                 } else {
-                    answer.append(Character.toLowerCase(c));
+                    arr[j] = arr[j].toLowerCase();
                 }
-                idx++;
+                answer.append(arr[j]);
+            }
+
+            // 단어 사이 공백 추가 (마지막 제외)
+            if (i != array.length - 1) {
+                answer.append(" ");
             }
         }
 
