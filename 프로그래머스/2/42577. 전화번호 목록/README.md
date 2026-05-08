bd006f0b8a7f034ec6e9bf7438baff76150f2ec3
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2026-05-09 01:22:31 +0900

    [level 2] Title: 전화번호 목록, Time: 189.16 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 217 MB, 시간: 293.47 ms
+메모리: 103 MB, 시간: 189.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 29일 01:03:01
+2026년 05월 09일 01:22:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,53 +1,18 @@
-// import java.util.*;
-// class Solution {
-//     public boolean solution(String[] phone_book) {
-//         boolean answer = true;
-//          Arrays.sort(phone_book);
-//         Set <String> set = new HashSet<>();
-        
-//         for(String phone : phone_book){
-//             set.add(phone);
-//              for(int i=0;i< phone_book.length;i++){ 
-//                  for(int j=0;j<phone.length();j++){
-//                      if(phone_book[i].equals(phone.substring(0,j))) {
-//                         System.out.println(answer);
-//                          return false;
-                         
-                       
-                         
-//                      };
-                     
-//                  }
-//              }
-//         }
-        
-//         return answer;
-//     }
-// }
-
-
 import java.util.*;
-
 class Solution {
     public boolean solution(String[] phone_book) {
-		    //✅ 해시셋에 phone_book의 전화번호를 모두 저장한다.
-	    	Set<String> prefixSet = new HashSet<>();
-        for (String number : phone_book) {
-            prefixSet.add(number);
-        }
+        boolean answer = true;
         
-        	    	
-	    	//✅ phone_book의 모든 전화번호를 순회한다.
-        for (String number : phone_book) {
-        		//✅ i를 1 ~ n까지 순회하며 0 ~ i 까지 부분문자열을 구한다.
-        	  for (int i = 1; i < number.length(); i++) {
-    	  				//✅ 부분문자열이 해시셋에 있다면 false를 반환한다.
-                String sub = number.substring(0, i);
-                if (prefixSet.contains(sub)) return false;
+        Arrays.sort(phone_book);
+        //System.out.print(phone_book);
+        
+        for(int i=0; i<phone_book.length-1;i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
             }
+            
         }
         
-        //✅ 접두어를 찾지 못했다면 true를 반환한다.
-        return true;
+        return answer;
     }
 }
