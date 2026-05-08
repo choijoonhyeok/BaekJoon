7e6ece4ba264bacfe81ae62b8fd36e1c8287e804
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2026-05-09 01:07:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.99 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 97.2 MB, 시간: 74.50 ms
+메모리: 102 MB, 시간: 43.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 22:12:42
+2026년 05월 09일 01:07:27
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,34 +1,24 @@
-import java.util.Scanner;
-import java.util.HashMap;
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String,Integer> hm =new HashMap<>();
-       
-        for(String player :participant){
-            hm.put(player,hm.getOrDefault(player,0)+1);
+        String answer ="";
+        Map <String,Integer> map = new HashMap<>();
+        
+        for(String part :participant){
+            map.put(part,map.getOrDefault(part,0)+1);
         }
         
-        for(String player :completion){
-            hm.put(player,hm.get(player)-1);
+        for(String comple:completion){
+            map.put(comple,map.getOrDefault(comple,0)-1);
         }
-         for(String key :hm.keySet()){
-            if(hm.get(key)>0) return key;
+        
+        for(String key : map.keySet()){
+            if(map.get(key)>0){
+                return key;
+            }
+         
         }
-        return "";
-    }
-    public static void main(String []args){
-        Scanner s = new Scanner(System.in);
-        
-        String str = s.nextLine();
-        str=str.replace("[\"","").replace("\"]","");
-        String participant[] = str.split("\", \"");
-        
-        String str2 = s.nextLine();
-        str2=str2.replace("[\"","").replace("\"]","");
-        String completion[] = str2.split("\", \"");
-        
-        Solution sol = new Solution();
-        System.out.print(sol.solution(participant,completion));
-        
+  
+        return answer;
     }
 }
