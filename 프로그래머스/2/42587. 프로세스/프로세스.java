1f3f9975c168630af1dccac71b25fa395d3058e6
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2026-05-09 00:20:41 +0900

    [level 2] Title: 프로세스, Time: 1.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,44 +1,44 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue <Integer> queue = new ArrayDeque<>();
         
-        Queue <int[]> queue = new ArrayDeque<>();
-        
-        for(int i=0; i<priorities.length;i++){
-            queue.offer(new int[]{i,priorities[i]});
+        for(int pri :priorities){
+            queue.offer(pri);
         }
         
        
-        int answer = 0;
+        int answerArr[] = new int[priorities.length];
         
-        while(!queue.isEmpty()){
-             // 1. 실행 대기 큐(Queue)에서 대기중인 프로세스 하나를 꺼냅니다.
-            int [] current = queue.poll();
-            
-            boolean hasHigher = false;
+        for(int i=0; i<priorities.length;i++){
+            // 1. 실행 대기 큐(Queue)에서 대기중인 프로세스 하나를 꺼냅니다.
+            int current = queue.poll();
+             int count = 1;
+           
             // 2. 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 방금 꺼낸 프로세스를 다시 큐에 넣습니다.
-            for(int[]next:queue){
-                if(next[1]>current[1]){
-                    hasHigher = true;
-                    break;
-                }
-            }
-            if(hasHigher){
-                queue.offer(current);
-            }
-            else{
-                answer++;
-                
-                if(current[0]==location){
-                    return answer;
-                }
-                
-            }
-            
-            
+             for(int j=i+1; j<priorities.length;j++){
+                 if(current<priorities[j]) {
+                     queue.offer(current);
+                     else count++;
+                     break;
+                 }
+                 
+             }
+// 3. 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스를 실행합니다.
+//   3.1 한 번 실행한 프로세스는 다시 큐에 넣지 않고 그대로 종료됩니다.
+           
+            answerArr[i]= count;
+              System.out.println(queue);
+             System.out.println(answerArr[i]);
         }
         
+        
 
-        return answer;
+        
+        
+        
+        
+        return answerArr[location];
     }
 }
