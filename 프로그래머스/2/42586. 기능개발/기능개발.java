63597a85442f4fe9fb607dfc228af907399dfb77
choijoonhyeok <151605441+choijoonhyeok@users.noreply.github.com>
2026-05-08 22:47:42 +0900

    [level 2] Title: 기능개발, Time: 2.56 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,30 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
-        
-        //완료 일 구하기
-        Queue <Integer> queue = new ArrayDeque<>();
-        for(int i=0;i<progresses.length;i++){
-            int endDay = (100-progresses[i]+speeds[i]-1)/speeds[i];
-            queue.offer(endDay);
-           
-        }
-        
-        
-       List <Integer>answerList = new ArrayList<>(); 
-    while(!queue.isEmpty()){
-        int now = queue.poll();
-        int count =1;
-        while(!queue.isEmpty()&&queue.peek()<=now){
-            queue.poll();
-            count++;
-        }
-    
-        answerList.add(count);
-    }
-         System.out.println(answerList);
-        
-        return answerList.stream().mapToInt(i->i).toArray();
+        return answer;
     }
 }
